d99fb1d8dfbd6d598408b428d4eaf90cad998f4c
CoreyStagner <cresc0428@gmail.com>
2025-01-04 21:32:30 -0500

    init commit converting to typescript. Still WIP

---
+++ next.config.js
@@ -0,0 +1,78 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    dirs: ['src'],
+  },
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://localhost:3000',
+          }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+
+  reactStrictMode: true,
+  // swcMinify: true,
+
+  // Uncoment to add domain whitelist
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'res.cloudinary.com',
+  //     },
+  //   ]
+  // },
+
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: { not: /\.(css|scss|sass)$/ },
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        loader: '@svgr/webpack',
+        options: {
+          dimensions: false,
+          titleProp: true,
+        },
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/components/Gantt/constants.js
@@ -0,0 +1,60 @@
+export const months = [
+  {
+    abbr: 'Jan',
+    long: 'January',
+  },
+  {
+    abbr: 'Feb',
+    long: 'February',
+  },
+  {
+    abbr: 'Mar',
+    long: 'March',
+  },
+  {
+    abbr: 'Apr',
+    long: 'April',
+  },
+  {
+    abbr: 'May',
+    long: 'May',
+  },
+  {
+    abbr: 'Jun',
+    long: 'June',
+  },
+  {
+    abbr: 'Jul',
+    long: 'July',
+  },
+  {
+    abbr: 'Aug',
+    long: 'August',
+  },
+  {
+    abbr: 'Sep',
+    long: 'September',
+  },
+  {
+    abbr: 'Oct',
+    long: 'October',
+  },
+  {
+    abbr: 'Nov',
+    long: 'November',
+  },
+  {
+    abbr: 'Dec',
+    long: 'December',
+  },
+];
+
+export const days = [
+  { abbr: 'Sun', long: 'Sunday', short: 'Su' },
+  { abbr: 'Mon', long: 'Monday', short: 'M' },
+  { abbr: 'Tue', long: 'Tuesday', short: 'Tu' },
+  { abbr: 'Wed', long: 'Wednesday', short: 'W' },
+  { abbr: 'Thu', long: 'Thursday', short: 'Th' },
+  { abbr: 'Fri', long: 'Friday', short: 'F' },
+  { abbr: 'Sat', long: 'Saturday', short: 'Sa' },
+];
